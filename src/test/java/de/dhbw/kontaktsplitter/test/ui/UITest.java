881af89d7857f3c12cac12c3984d3ae414062150
Lukas Lautenschlager <lukas.lautenschlager@outlook.de>
2020-05-10 14:13:54 +0200

    Initial UI Commit

--- src/test/java/de/dhbw/kontaktsplitter/test/ui/UITest.java
+++ src/test/java/de/dhbw/kontaktsplitter/test/ui/UITest.java
@@ -1,13 +1,20 @@
 package de.dhbw.kontaktsplitter.test.ui;
 
+import de.dhbw.kontaktsplitter.models.Gender;
+import de.dhbw.kontaktsplitter.persistence.Configuration;
 import de.dhbw.kontaktsplitter.ui.Startup;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxAssert;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationExtension;
 import org.testfx.framework.junit5.Start;
+import org.testfx.matcher.control.ComboBoxMatchers;
+import org.testfx.matcher.control.LabeledMatchers;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -32,5 +39,14 @@ public class UITest {
     @Test
     public void test(FxRobot robot) throws InterruptedException {
         assertTrue(true);
+    }
+
+    @Test
+    void initializeUI_correct_setup(FxRobot robot)
+    {
+        FxAssert.verifyThat("#lbl_salutation", LabeledMatchers.hasText(""));
+        FxAssert.verifyThat("#cmb_gender", ComboBoxMatchers.containsItems(Gender.values()));
+        FxAssert.verifyThat("#cmb_title", ComboBoxMatchers.containsItems(Configuration.getTitles()));
+        FxAssert.verifyThat("#cmb_language", ComboBoxMatchers.containsItems(List.of("DE", "EN")));
     }
 }

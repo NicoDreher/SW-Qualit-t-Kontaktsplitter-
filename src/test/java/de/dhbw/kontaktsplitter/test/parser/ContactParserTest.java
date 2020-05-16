7ff6bcfe8bf5dc9b0db76dce9178b5520990d28d
Nico Dreher <Nico.Dreher98@gmail.com>
2020-05-16 21:33:22 +0200

    Fixed failing Test finally...

--- src/test/java/de/dhbw/kontaktsplitter/test/parser/ContactParserTest.java
+++ src/test/java/de/dhbw/kontaktsplitter/test/parser/ContactParserTest.java
@@ -111,7 +111,7 @@ public class ContactParserTest {
      * @param expectedOutput The expected output
      */
     @ParameterizedTest(name = "[{index}] Input {0}")
-    @CsvFileSource(resources = "/parser/output.csv")
+    @CsvFileSource(resources = "/parser/outputs.csv")
     void testOutput(String input, String expectedOutput) {
         assertEquals(expectedOutput, InputParser.generateOutput(InputParser.parseInput(input)));
     }
